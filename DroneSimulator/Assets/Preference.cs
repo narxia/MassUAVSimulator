15d82ad3463fbdcad4bdd4d28686fc04777dae09
SOOHAN-PC\narxia <narxia88@gmail.com>
2018-10-11 15:35:50 +0900

    Area Object 동적 생성하도록 소스 수정

--- DroneSimulator/Assets/Preference.cs
+++ DroneSimulator/Assets/Preference.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
-using UnityEditor;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+
+[System.Serializable]
 public class Area
 {
-	Vector3 Point_Start;
-	Vector3 Point_End;
+	public Vector2 Point_Start;
+	public Vector2 Point_End;
+	public Area(Vector2 start,Vector2 end)
+	{
+		Point_Start = start;
+		Point_End = end;
+	}
 }
 public class Preference : MonoBehaviour {
-[CustomEditor(typeof(Preference))]
-[CanEditMultipleObjects]
-//public class Preference : Editor {
-	public List<Area> AreaList; //Drone Data List
+	public List<Area> AreaList; //
+	public List<float> ClassList; 
 	public Vector3 World_size;
-	private Vector3 before_width;
-
+	private Vector3 before_size;
+	private List<GameObject> AreaObjctList;
+	public bool bRefresh;
 
 	GameObject Floor;
 	GameObject Sky;
@@ -23,18 +30,29 @@ public class Preference : MonoBehaviour {
 		World_size.x = 1000;
 		World_size.y = 1000;
 		World_size.z = 1000;
+
+		AreaList = new List<Area> ();
+		ClassList = new List<float> ();
+		AreaObjctList = new List<GameObject> ();
+
 		Floor = GameObject.Find ("Floor");	
 		Sky = GameObject.Find ("Height");	
 
-		Debug.Log (Floor.transform.localScale.x);
-		Debug.Log (Floor.transform.localScale.y);
-		Debug.Log (Floor.transform.localScale.z);
+		AreaList.Clear ();
+
+		ClassList.Clear ();
+		AreaObjctList.Clear ();
+		bRefresh = true;
+		//todo: Test
+		AreaList.Add(new Area(new Vector2(0,0),new Vector2(100,100)));
+		ClassList.Add (100);
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
-		if ( World_size!= before_width) {
+		//Floor,Sky Setting
+		if ( World_size!= before_size) {
 			if (Floor.transform.localScale.x != World_size.x || Floor.transform.localScale.z != World_size.y) {
 				Floor.transform.localScale = new Vector3 (World_size.x, 1, World_size.y);	
 				Floor.transform.position = new Vector3 (0, 0, World_size.x / 2);
@@ -43,10 +61,32 @@ public class Preference : MonoBehaviour {
 				Sky.transform.localScale = new Vector3 (World_size.x, World_size.z,1 );	
 				Sky.transform.position = new Vector3 (0, 0, World_size.y);
 			}
-			before_width = World_size;
+			before_size = World_size;
 		}
+		//Area Making
+		if (AreaList.Count > 0 && ClassList.Count > 0) {
+			if (bRefresh) {
+				bRefresh = false;
+				//Clear Object
+				for (int i=0; i < AreaObjctList.Count; i++) {
+					Destroy (AreaObjctList [i]);	
+				}
+				AreaObjctList.Clear ();
+				//Create Object
+				for(int i=0;i<AreaList.Count;i++)
+				{					
+					GameObject Create_Obj;
+					GameObject Org_Obj;
+					float width = Math.Abs(AreaList[i].Point_End.x-AreaList[i].Point_Start.x);
+					float height= Math.Abs(AreaList[i].Point_End.y-AreaList[i].Point_Start.y);
+					Org_Obj = GameObject.Find ("Area");
+					Create_Obj = Instantiate (Org_Obj, new Vector3 (AreaList[i].Point_Start.x,AreaList[i].Point_Start.y,1), Org_Obj.transform.rotation);	
+					Create_Obj.transform.localScale = new Vector3 (width,100 , height);
+					AreaObjctList.Add (Create_Obj);
 
-
+				}
+			}
+		}
 	}
 		
 
